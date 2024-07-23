40ebd9a11da9949b670e4b08dbc54f5fed5ad4a0
EC2 Default User <ec2-user@ip-172-31-33-104.eu-west-3.compute.internal>
2024-07-23 08:44:47 +0000

    add test

---
+++ tests/test_main.py
@@ -0,0 +1,14 @@
+from fastapi.testclient import TestClient
+
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from main import app
+
+client = TestClient(app)
+
+def test_ping():
+    response = client.get("/ping")
+    assert response.status_code == 200
+    assert response.text == '"pong"'
